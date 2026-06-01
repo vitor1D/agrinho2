3e4c1314e10c32d607896669d902dd633f97d219
vitormateus1D <165968433+vitor1D@users.noreply.github.com>
2026-06-01 19:49:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const botao = document.getElementById("btnMensagem");
+const mensagem = document.getElementById("mensagem");
 
+botao.addEventListener("click", () => {
+    mensagem.textContent =
+        "🌎 Um agro forte é aquele que produz alimentos com responsabilidade, inovação e respeito ao meio ambiente.";
+});
